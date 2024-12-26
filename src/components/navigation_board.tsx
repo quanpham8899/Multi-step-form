fc3d63b51bf994fc9e558113560c18c7d9d55e94
The Handsome Duke <p.hoangquan8899@gmail.com>
2024-12-26 14:28:50 +0700

    Something

--- src/components/navigation_board.tsx
+++ src/components/navigation_board.tsx
@@ -8,25 +8,27 @@ export interface ButtonListProps {
 }
 
 const NavigationBoard: React.FC<ButtonListProps> = ({ data }) => {
-  const [width, setWidth] = React.useState(window.innerWidth);
+  const [width, setWidth] = React.useState<number | null>(null);
 
   React.useEffect(() => {
-    const handleResize = () => {
-      setWidth(window.innerWidth);
-    };
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWidth(window.innerWidth);
+      };
 
-    // Add event listener when component mounts
-    window.addEventListener("resize", handleResize);
+      // Add event listener when component mounts
+      window.addEventListener("resize", handleResize);
 
-    // Cleanup event listener when component unmounts
-    return () => {
-      window.removeEventListener("resize", handleResize);
-    };
+      // Cleanup event listener when component unmounts
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
   }, []);
 
   return (
     <div
-      className={`bg-purplish-blue background-desktop ${width >= 1280 ? "background-desktop" : "background-mobile"} pt-10 rounded-lg flex 
+      className={`bg-purplish-blue background-desktop ${((width !== null && width >= 1280) || width === null) ? "background-desktop" : "background-mobile"} pt-10 rounded-lg flex 
         flex-col gap-6 px-4 pr-10 
         lg:px-8 lg:pr-20 lg:min-w-72 max-xl:flex-row max-sm:p-12 max-xl:justify-center max-xl:py-20 max-xl:gap-12 max-sm:rounded-none`}>
       {data.filter((d) => d.step < 5).map((d) => (
